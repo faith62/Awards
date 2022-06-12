91f9aeb44a7e1c2f61a36cda5a65255fc93d68b1
faith62 <phifeichemu@gmail.com>
2022-06-12 20:04:06 +0300

    add models

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+from django.urls import reverse
+
+def user_directory_path(instance, filename):
+    #this file will be uploaded to MEDIA_ROOT/user(id/filename)
+    return 'user_{0}/{1}'.format(instance.user.id,filename)
 
 # Create your models here.
+class Image(models.Model):    
+    image_name =models.CharField(max_length=50)
+    image_caption =models.CharField(max_length=50)
+    pic=models.ImageField(upload_to=user_directory_path,blank=True,null = True)
+    # post = HTMLField(blank=True,null = True,)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null = True,)
+    post_date = models.DateTimeField(auto_now_add=True,blank=True,null = True,)
+    likes = models.IntegerField(blank=True,null = True,)
+    url=models.CharField(max_length=50,blank=True,null = True,)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_absolute_url(self):
+        return reverse('imagedetails',args=[str(self.id)])#clickto image get to imagedetails url
+
+    class Meta:
+        ordering = ['image_name']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True,null = True,)
+    first_name = models.CharField(max_length=50,blank=True,null = True,)
+    last_name = models.CharField(max_length=50,blank=True,null = True,)
+    bio=models.CharField(max_length=50)
+    profile_photo=models.ImageField(upload_to='profile/',blank=True,null = True,)
+    url=models.CharField(max_length=50,blank=True,null = True,)
+    created =models.DateField(auto_now_add=True,blank=True,null = True,)
+
+    def __str__(self):
+        return self.first_name
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    def save_user_profile(sender,instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+    post_save.connect(create_user_profile, sender=User)
+    post_save.connect(save_user_profile, sender=User)
+
