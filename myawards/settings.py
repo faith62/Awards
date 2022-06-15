ac7bc2e07c09979d7a9d853fd93e44870932623e
faith62 <phifeichemu@gmail.com>
2022-06-15 10:31:54 +0300

    hroku deploy

--- myawards/settings.py
+++ myawards/settings.py
@@ -173,12 +173,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
  
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dgrjyra6k',
